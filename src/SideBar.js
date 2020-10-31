8a44656015b0c19d35a0fad3f1a8c5093223bd67
jito <jitokj3@gmail.com>
2020-10-31 22:50:51 +0530

    sidebar header completed

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import "./SideBar.css";
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar,IconButton } from "@material-ui/core";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import logo from "./images/man.png";
+
 
 const SideBar = () => {
     return (
         <div className="sidebar">
-            <h1>SideBar</h1>
+            
             <div className="sidebar__header">
-                <div className="sidebar__headerRight"></div>
+            <Avatar src={logo} />
+           
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
         </div>
     );

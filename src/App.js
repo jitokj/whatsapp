4048f3fb79c531d210a78282c3d10813c3bc244e
jito <jitokj3@gmail.com>
2020-10-31 19:28:05 +0530

    added chat and sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './App.css';
+import Chat from "./Chat";
+import SideBar from "./SideBar";
 
 function App() {
   return (
     <div className="app">
-    <h1>Whatsapp</h1>
+    
+    {/* {sideBar component} */}
+    <SideBar /> 
+    {/* {chat Comonent} */}
+    <Chat />
     </div>
   );
 }

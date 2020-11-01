50ea484c61972632736bc0a6230ca9ea116fcaea
jito <jitokj3@gmail.com>
2020-11-01 12:10:25 +0530

    added chat body

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,52 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 import React from 'react';
 import "./Chat.css";
 
 const Chat = () => {
     return (
         <div className="chat">
-            <h1>Chat</h1>
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+               <div className="chat__headerRight">
+                   <IconButton>
+                       <SearchOutlined />
+                   </IconButton>
+                   <IconButton>
+                       <AttachFile />
+                   </IconButton>
+                   <IconButton>
+                       <MoreVert />
+                   </IconButton>
+               </div>
+            </div>
+            <div className="chat__body">
+            <p className="chat__message">
+            <span className="chat__name">jito</span>
+                This is a message
+                <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+            <span className="chat__name">jito</span>
+                This is a reciever message
+                <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">Send Messages</button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     );
 };

9bc081332f765a9b0cf60bd93a402a2dcea47ad3
jito <jitokj3@gmail.com>
2020-11-02 11:58:18 +0530

    connected with database and realtime push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import './App.css';
 import Chat from "./Chat";
 import SideBar from "./SideBar";
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+
+  const [messages,setMessages] = useState([]);
+
+  useEffect(()=>{
+    axios.get("/messages/sync")
+    .then(response=>{
+     
+      setMessages(response.data);
+    })
+    .catch(err=>console.log(err));
+  },[])
+
   useEffect(()=>{
     var pusher = new Pusher('958aadfaf52da2758d3f', {
       cluster: 'ap2'
@@ -16,6 +29,9 @@ function App() {
       alert(JSON.stringify(data));
     });
   },[])
+
+
+  console.log(messages);
   return (
     <div className="app">
     <div className="app__body">
